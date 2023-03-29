040798f5cac97d1f471c9b285f1a841c33c6b6c7
Erick Labrada <Erick.labrada233380@potros.itson.edu.mx>
2023-03-29 11:49:35 -0700

    <Primer commit> Creación del repositorio, proyecto e importación de las bibliotecas "MySQLConnector", "LGoodDatePicker" y "JasperReports" utilizando maven

---
+++ src/main/java/com/mycompany/proyecto02/basesdedatosavanzadas/Proyecto02BasesDeDatosAvanzadas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto02.basesdedatosavanzadas;
+
+/**
+ *
+ * @author Erick
+ */
+public class Proyecto02BasesDeDatosAvanzadas {
+
+    public static void main(String[] args) {
+        
+    }
+}
